2f9768c607fae5333e8944bc47881d94214faeae
SeungkeyMouse <ksg19980@gmail.com>
2022-02-03 23:54:24 +0900

    REST API 까지 공부완료(Put,Get,Post,Delete 어노테이션 및 메소드)

---
+++ src/main/java/com/thinkground/aroundhub/AroundhubApplication.java
@@ -0,0 +1,13 @@
+package com.thinkground.aroundhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkground/aroundhub/AroundhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkground.aroundhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
